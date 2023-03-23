7ee17e4d48e7b00327cc468a2f83947cf7013f6c
VaibhaVnagre <nagrevaibhav001@gmail.com>
2023-03-24 00:08:24 +0530

    ReactExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItem from './Expense';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <ExpenseItem></ExpenseItem>
+          
         </p>
         <a
           className="App-link"
