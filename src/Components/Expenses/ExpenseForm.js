05f63d8ca5f9737f212ff32fe408f76242cf9513
VaibhaVnagre <nagrevaibhav001@gmail.com>
2023-04-02 22:39:21 +0530

    Set Input value to varible ..Onchange

--- src/Components/Expenses/ExpenseForm.js
+++ src/Components/Expenses/ExpenseForm.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 
 import './ExpenseForm.css'
 
 function ExpenseForm (){
-   
-   function ChangeHandeler (e){
-    console.log(e.target.value)
-   }
+
+  const [enteredtitle, setEnteredtitle] = useState("");
+  const [enteredAmount, setEnteredAmount]= useState('');
+  const [enteredDate, setEnteredDate]= useState('');
+
+   const ChangeTitleHandeler =(e)=>{
+    setEnteredtitle(e.target.value);
+    console.log(enteredtitle)
+   };
+
+   const ChangeAmmountHandeler=(e)=>{
+    setEnteredAmount(e.target.value);
+   };
+
+   const ChangeDateHandeler =(e)=>{
+    setEnteredDate(e.target.value);
     
+   }
    
     
     return(
@@ -16,17 +29,17 @@ function ExpenseForm (){
 
   <div className="new-expense__control label">
         <label>Title</label>
-        <input type="text" onChange={ChangeHandeler}/>
+        <input type="text" onChange={ChangeTitleHandeler}/>
   </div>
         
   <div className="new-expense__control label">
         <label>Ammount</label>
-        <input type="number" onChange={ChangeHandeler}/>
+        <input type="number" onChange={ChangeAmmountHandeler}/>
   </div>  
 
   <div className="new-expense__control label">
         <label>Date</label>
-        <input type="date" min="2019-01-01" max="2020-12-31" onChange={ChangeHandeler}/>
+        <input type="date" min="2019-01-01" max="2020-12-31" onChange={ChangeDateHandeler}/>
   </div> 
 
   <div className="new-expense__actions">
